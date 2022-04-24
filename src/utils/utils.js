addcfab2b9bd858ecd85bebdeb8f1b4d3d4ec14f
Alexander <raskalovalexander@gmail.com>
2022-04-24 21:21:06 +0300

    fix url

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
   URL_MOVIES: "https://api.nomoreparties.co/beatfilm-movies",
-  URL_API: "http://localhost:3000",
+  URL_API: "https://api.news-explorer.nomoredomains.work",
   URL_IMAGE: "https://api.nomoreparties.co",
 }
 
